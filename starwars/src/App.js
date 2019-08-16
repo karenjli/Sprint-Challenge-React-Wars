b7d7a10cf8037506b014e1006a9a5b3bdc001925
karenjli <karenjy.li@gmail.com>
2019-08-16 11:43:19 -0500

    all first page data passed in

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
 import './App.css';
+import CharacterCard from "./components/CharacterCard"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+    // const [name, setName] = useState ([]);
+    // const [height, setHeight] = useState ([]);
+    // const [mass, setMass] = useState ([]);
+    // const [haircolor, setHaircolor] = useState ([]);
+    // const [skincolor, setSkincolor] = useState ([]);
+    // const [eyecolor, setEyecolor] = useState ([]);
+    // const [birthyear, setBirthyear] = useState ([]);
+    // const [gender, setGender] = useState ([]);
+    const [page, setPage] = useState (1);
+    const [results, setResults] = useState ([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+    useEffect (() => {
+      axios.get (`https://swapi.co/api/people/?page${page}`)
+        .then(response => {
+          console.log(response);
+          setResults(response.data.results);
+          
+        })
+        .catch (err => {
+          console.log(err)
+        });
+    }, [])
+  
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {results.map (char => <CharacterCard name={char.name} height = {char.height} mass = {char.mass} hair={char.hair_color} skin={char.skin_color} eye={char.eye_color} birth={char.birth_year} gender={char.gender} />)}
     </div>
+     
+    
   );
 }
 
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,13 +4,15 @@ export default function CharacterCard (props) {
     return (
         <div className="card">
             <h2>{props.name}</h2>
-            <p>Height: {props.height}</p>
-            <p>Mass: {props.mass}</p>
-            <p>Hair Color: {props.hair_color}</p>
-            <p>Skin Color: {props.skin_color}</p>
-            <p>Eye Color: {props.eye_color}</p>    
-            <p>Birth Year: {props.birth_year}</p>
-            <p>Gender: {props.gender}</p>
+            <div className="qualities">
+                <p id="height">Height: {props.height}</p>
+                <p id="mass">Mass: {props.mass}</p>
+                <p id="hair">Hair Color: {props.hair}</p>
+                <p id="skin">Skin Color: {props.skin}</p>
+                <p id="eye">Eye Color: {props.eye}</p>    
+                <p id="birth">Birth Year: {props.birth}</p>
+                <p id="gender">Gender: {props.gender}</p>
+            </div>
         </div>
     )
 }
